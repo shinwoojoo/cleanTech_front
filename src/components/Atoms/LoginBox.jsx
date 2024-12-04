9c94cedfdf00e8c5a4302283e5500ca6a2098b1a
sw347 <>
2024-12-04 16:26:45 +0900

    google login setting

--- src/components/Atoms/LoginBox.jsx
+++ src/components/Atoms/LoginBox.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 // import { GoogleLogin, useGoogleLogin } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
+import {useGoogleLogin} from "@react-oauth/google";
 // import { useEffect } from "react";
 
 // const REDIRECT_URI = "http://localhost:3124/main";
@@ -27,31 +28,9 @@ const ButtonBox = styled.div`
 `;
 
 const Login = () => {
-  const navigate = useNavigate();
-
-  // const fetchUserInfo = async (accessToken) => {
-  //   try {
-  //     localStorage.setItem("authToken", accessToken);
-  //     fetch("http://localhost:3125/auth/google", {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({ token: response.credential }),
-  //     }).catch((err) => console.error(err));
-  //     navigate("/main");
-  //   } catch (error) {
-  //     console.log(`error: `, error);
-  //   }
-  // };
-
-  // const handleLoginSuccess = useGoogleLogin({
-  //   scope: "email profile",
-  // });
-
-  const handleLoginSuccess = () => {
-    navigate("/main");
-  };
+    const handleGoogleLogin = () => {
+      window.location.href = "http://localhost:3125/auth/google";
+  }
 
   return (
     <>
@@ -60,13 +39,13 @@ const Login = () => {
           src="../src/assets/stuLogin.png"
           alt="학생으로 로그인"
           style={{ width: "240px" }}
-          onClick={handleLoginSuccess}
+          onClick={handleGoogleLogin}
         />
         <img
           src="../src/assets/teachLogin.png"
           alt="선생님으로 로그인"
           style={{ width: "240px" }}
-          onClick={handleLoginSuccess}
+          onClick={handleGoogleLogin}
         />
       </ButtonBox>
     </>
